24fb6110c903ac61db3547878cd0958f98a7b36c
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-08-27 15:40:40 +0500

    Added the Spearman class

--- src/Characters/Spearman.java
+++ src/Characters/Spearman.java
@@ -1,2 +1,91 @@
-package Characters;public class Spearman {
+package Characters;
+
+import java.util.Random;
+
+public class Spearman {
+    private static int number;
+    private static Random r;
+
+    private String name;
+    private int attack;
+    private int protection;
+    private int shotsFired;
+    private int[] damage;
+    private int health;
+    private int speed;
+    private boolean delivery;
+    private boolean magic;
+
+    static {
+        Spearman.number = 0;
+        Spearman.r = new Random();
+    }
+
+
+    /**
+     * Конструктор создания персонажа 1.
+     * @param name Имя персонажа заданного пользователем.
+     */
+    public Spearman(String name) {
+        this.name = name;
+
+        this.attack = 4;
+        this.protection = 5;
+        this.shotsFired = 0;
+        this.damage = new int[]{1, 3, 0};
+        this.health = 10;
+        this.speed = 4;
+        this.delivery = false;
+        this.magic = false;
+    }
+
+    /**
+     * Конструктор создания персонажа 2.
+     * @param name Имя персонажа заданного пользователем.
+     * @param attack Увеличенная атака.
+     */
+    public Spearman(String name, int attack) {
+        this(name);
+        this.attack = 5;
+    }
+
+    public Spearman() {
+        this(String.format("Hero #%d", ++Spearman.number));
+        this.damage[2] = Spearman.r.nextInt(damage[0],damage[1] + 1);
+    }
+
+
+    /** Геттер приватного поля name */
+    public String getName() {return this.name;}
+
+    /** Геттер приватного поля attack */
+    public int getAttack() {return this.attack;}
+
+    /** Геттер приватного поля protection */
+    public int getProtection() {return this.protection;}
+
+    /** Геттер приватного поля shotsFired */
+    public int getShotsFired() {return this.shotsFired;}
+
+    /** Геттер приватного поля damage */
+    public int[] getDamage() {return this.damage;}
+
+    /** Геттер приватного поля health */
+    public int getHealth() {return this.health;}
+
+    /** Геттер приватного поля speed */
+    public int getSpeed() {return this.speed;}
+
+    /** Геттер приватного поля delivery */
+    public boolean getDelivery() {return this.delivery;}
+
+    /** Геттер приватного поля magic */
+    public boolean getMagic() {return this.magic;}
+
+
+    /** Краткое описание персонажа */
+    public String getInfo() {
+        return String.format("Name: %s, health: %d, damage: %d, type: %s.",
+                this.name, this.health, this.damage[2], this.getClass().getSimpleName());
+    }
 }
